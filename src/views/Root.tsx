d958c70d51330b95ef6e4075302a302fc7e345f2
Rafal <rafek131@wp.pl>
2024-10-09 14:33:32 +0200

    mobile navigation is now closed after route is changed

--- src/views/Root.tsx
+++ src/views/Root.tsx
@@ -1,16 +1,21 @@
-import { useState } from 'react';
-import { Outlet } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Outlet, useLocation } from 'react-router-dom';
 import { GlobalStyle } from 'src/assets/styles/GlobalStyle';
 import { MainTemplate } from 'src/components/templates/MainTemplate/MainTemplate';
 import { Header } from 'src/components/atoms/Header/Header';
 import { NavBar } from 'src/components/organisms/NavBar/NavBar';
 
 export const Root = () => {
+	const currentRoute = useLocation();
 	const [isNavActive, setNavState] = useState(false);
 
 	const handleMobileNav = () => setNavState(prevState => !prevState);
 
 	const closeMobileNav = () => setNavState(false);
+
+	useEffect(() => {
+		closeMobileNav();
+	}, [currentRoute]);
 
 	return (
 		<div>
